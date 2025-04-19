ae0a89d935702094f1cf91960dbfa81ff8f812ba
Steven Dao <stevedao@gmail.com>
2025-04-18 23:16:43 -0500

    working state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,262 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import cloudflareLogo from './assets/Cloudflare_Logo.svg'
 import './App.css'
 
+type WeatherData = { description: string, temperature: number };
+type RouteNamesEntry = { routeId: string, directions: { directionId: number, name: string }[] };
+type BusTimesEntry = { stopId: string, routes: { routeId: string, directions: { directionId: number, nextTimes: number[] }[] }[] };
+
+function Weather({ weather } : { weather: null | { description: string, temperature: number }})
+{
+  if (weather === null)
+  {
+    return (
+      <>
+      </>
+    )
+  }
+  else
+  {
+    return (
+      <header>
+        <h2>{weather.description}</h2>
+        <h3>{Math.round(weather.temperature * (9.0/5.0) + 32.0)}&deg;F</h3>
+      </header>
+    )
+  }
+}
+
+function MinutesDisplay({ minutes } : { minutes: number})
+{
+  if (minutes <= 10)
+  {
+    return <span className="imminent">{minutes}</span>
+  }
+  else if (minutes <= 15)
+  {
+    return <span className="verySoon">{minutes}</span>
+  }
+  else if (minutes <= 20)
+  {
+    return <span className="soon">{minutes}</span>
+  }
+  else
+  {
+    return <span>{minutes}</span>
+  }
+}
+
+function BusTime({ routeId, directionId, nextTimes, routeNames } : { routeId: string, directionId: number, nextTimes: number[], routeNames: RouteNamesEntry[] })
+{
+  const [_, setCount] = useState(0);
+  
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setCount((val) => val + 1);
+    }, 5 * 1000);
+
+    return () => window.clearInterval(intervalId);
+  }, []);
+
+  var name = `Route ${routeId}`;
+  const entry = routeNames.find((entry) => entry.routeId == routeId);
+  if (entry !== undefined)
+  {
+    const dir = entry.directions.find((dir) => dir.directionId == directionId);
+    if (dir !== undefined)
+    {
+      name = dir.name;
+    }
+  }
+
+  // const dirCode = directionId === 1 ? "N" : "S";
+
+  const nowSeconds = Date.now() / 1000.0;
+  const minutesUntil = nextTimes.map((seconds) => Math.floor((seconds - nowSeconds) / 60.0)).filter((minutes) => minutes >= 0.0).sort((a, b) => a - b).slice(0, 2);
+
+  const countdowns = [];
+  for (var i = 0; i < minutesUntil.length; ++i)
+  {
+    if (i !== 0)
+    {
+      countdowns.push(<span>,&nbsp;</span>);
+    }
+    countdowns.push(<MinutesDisplay minutes={minutesUntil[i]} />);
+  }
+
+  const className = String(routeId).length > 2 ? "route route3" : "route";
+
+  return (
+    <article>
+        <h1><em className={className}><span>{routeId}</span></em></h1>
+        <h2>{name}</h2>
+        <h3>{countdowns} <div className="subtitle">minutes</div></h3>
+    </article>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState('unknown')
+  // const [count, setCount] = useState(0);
+  // const [name, setName] = useState('unknown');
+
+  const [weather, setWeather] = useState<null | WeatherData>(null);
+  const [routeNames, setRouteNames] = useState<null | RouteNamesEntry[]>(null);
+  const [busTimes, setBusTimes] = useState<null | BusTimesEntry[]>(null);
+  
+  // useEffect(() => {
+  //   const intervalId = window.setInterval(() => {
+  //     setCount((val) => val + 1);
+  //   }, 1000);
+
+  //   return () => window.clearInterval(intervalId);
+  // }, []);
+
+  useEffect(() => {
+    var timeoutId = 0;
+    const fetchRouteNames = async () => {
+      console.log("Fetching route names...");
+
+      try
+      {
+        const response = await fetch('/routenames');
+        const data = await response.json();
+        const { ok, routes } = data;
+        if (ok === true) {
+          setRouteNames(routes);
+          return true;
+        }
+      }
+      catch (e)
+      {
+      }
+
+      timeoutId = window.setTimeout(() => fetchRouteNames(), 30 * 1000);
+      return false;
+    };
+
+    fetchRouteNames();
+    return () => window.clearTimeout(timeoutId);
+  }, []);
+
+  useEffect(() => {
+    var timeoutId = 0;
+    const paramsString = window.location.search;
+    const searchParams = new URLSearchParams(paramsString);
+    const stopIds = searchParams.get("stopids");
+
+    if (stopIds !== null)
+    {
+      const fetchBusTimes = async () => {
+        console.log("Fetching bus times...");
+
+        var nextTimeout = 60 * 1000;
+        try
+        {
+          const response = await fetch(`/bustimes?stops=${stopIds}`);
+          const data = await response.json();
+          const { ok, stops } = data;
+          if (ok === true)
+          {
+            setBusTimes(stops);
+
+            // If there are no more scheduled buses for the night, throttle updates to every ten minutes
+            const isAnyNextTime = () => {
+              if (stops !== null)
+              {
+                for (const stop of (stops as BusTimesEntry[]))
+                {
+                  for (const route of stop.routes)
+                  {
+                    for (const dir of route.directions)
+                    {
+                      if (dir.nextTimes.length !== 0)
+                      {
+                        return true;
+                      }
+                    }
+                  }
+                }
+              }
+              return false;
+            };
+
+            if (!isAnyNextTime())
+            {
+              nextTimeout = 10 * 60 * 1000;
+            }
+          }
+        }
+        catch (e)
+        {
+        }
+
+        timeoutId = window.setTimeout(() => fetchBusTimes(), nextTimeout);
+      };
+
+      fetchBusTimes();
+    }
+
+    return () => window.clearTimeout(timeoutId);
+  }, []);
+
+  useEffect(() => {
+    var timeoutId = 0;
+    const paramsString = window.location.search;
+    const searchParams = new URLSearchParams(paramsString);
+    const weatherStation = searchParams.get("weather");
+
+    if (weatherStation !== null)
+    {
+      const fetchWeather = async () => {
+        console.log("Fetching weather...");
+
+        try
+        {
+          const response = await fetch(`/weather?station=${weatherStation}`);
+          const data = await response.json();
+          const { ok, weather } = data;
+          if (ok === true)
+          {
+            setWeather(weather);
+          }
+        }
+        catch (e)
+        {
+        }
+
+        timeoutId = window.setTimeout(() => fetchWeather(), 15 * 60 * 1000);
+      };
+
+      fetchWeather();
+    }
+
+    return () => window.clearTimeout(timeoutId);
+  }, []);
+
+  const rows = [];
+  if (busTimes !== null && routeNames !== null)
+  {
+    for (const stop of busTimes)
+    {
+      for (const route of stop.routes)
+      {
+        for (const dir of route.directions)
+        {
+          rows.push(<BusTime routeId={route.routeId} directionId={dir.directionId} nextTimes={dir.nextTimes} routeNames={routeNames} />);
+        }
+      }
+    }
+  }
+  if (rows.length == 0)
+  {
+    rows.push(<article><h2>Nothing scheduled</h2></article>);
+  }
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href='https://vite.dev' target='_blank'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
@@ -32,25 +278,45 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
-      <div className='card'>
+      </div> */}
+      {/* <div className='card'>
         <button
           onClick={() => {
-            fetch('/api/')
-              .then((res) => res.json() as Promise<{ name: string }>)
-              .then((data) => setName(data.name))
+            fetch('/weather?station=KAUS')
+              .then((res) => res.json())
+              .then((data) => {
+                const { ok, weather } = data;
+                if (ok === true) {
+                  setWeather(weather);
+                }
+              });
+            fetch('/bustimes?stops=3757,2628,4382')
+              .then((res) => res.json())
+              .then((data) => {
+                const { ok, stops } = data;
+                if (ok === true) {
+                  setBusTimes(stops);
+                }
+              });
+            fetch('/routenames')
+              .then((res) => res.json())
+              .then((data) => {
+                const { ok, routes } = data;
+                if (ok === true) {
+                  setRouteNames(routes);
+                }
+              });
           }}
           aria-label='get name'
         >
-          Name from API is: {name}
+          Click to Update Data
         </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      </div> */}
+      
+      <Weather weather={weather} />
+      <main>
+        {rows}
+      </main>
     </>
   )
 }
--- worker/index.ts
+++ worker/index.ts
@@ -1,11 +1,211 @@
+import JSZip from "jszip";
+
+async function getBusTimes(stops: string[])
+{
+  const response = await fetch("https://data.texas.gov/download/mqtr-wwpy/text%2Fplain");
+  const { headers } = response;
+  const contentType = headers.get("content-type") || "";
+  if (contentType.includes("application/octet-stream"))
+  {
+    try
+    {
+      const jsonString = new TextDecoder("utf-8").decode(await response.arrayBuffer());
+      const payload = JSON.parse(jsonString);
+
+      const arrivals: Map<string, Map<string, Map<number, number[]>>> = new Map();
+      for (const stopId of stops)
+      {
+        arrivals.set(stopId, new Map());
+      }
+
+      function getTimesArray(stopId: string, routeId: string, directionId: number): number[] | undefined
+      {
+        const arrivalsStop = arrivals.get(stopId);
+        if (arrivalsStop === undefined)
+        {
+          return undefined;
+        }
+
+        var arrivalsRoute = arrivalsStop.get(routeId);
+        if (arrivalsRoute === undefined)
+        {
+          arrivalsRoute = new Map();
+          arrivalsStop.set(routeId, arrivalsRoute);
+        }
+
+        var arrivalsDirection = arrivalsRoute.get(directionId);
+        if (arrivalsDirection === undefined)
+        {
+          arrivalsDirection = [];
+          arrivalsRoute.set(directionId, arrivalsDirection);
+        }
+
+        return arrivalsDirection;
+      }
+
+      for (const entity of payload.entity)
+      {
+        const { trip, stopTimeUpdate } = entity.tripUpdate;
+        if (trip !== undefined && stopTimeUpdate !== undefined)
+        {
+          const { routeId, directionId } = trip;
+          for (const update of stopTimeUpdate)
+          {
+            const { arrival, stopId, scheduleRelationship } = update;
+            if (scheduleRelationship === "SCHEDULED")
+            {
+              const timesArray = getTimesArray(stopId, routeId, directionId);
+              if (timesArray !== undefined)
+              {
+                const time = parseInt(arrival.time);
+                timesArray.push(time);
+              }
+            }
+          }
+        }
+      }
+
+      const niceStops = Array.from(arrivals, ([stopId, routes]) => {
+        const niceRoutes = Array.from(routes, ([routeId, directions]) => {
+          const niceDirections = Array.from(directions, ([directionId, times]) => {
+            const nextTimes = times.sort((a, b) => a - b).slice(0, 5);
+            return { directionId, nextTimes };
+          });
+          return { routeId, directions: niceDirections };
+        });
+        return { stopId, routes: niceRoutes };
+      });
+
+      return { ok: true, stops: niceStops };
+    }
+    catch (e)
+    {
+    }  
+  }
+
+  return { ok: false, stops: null };
+}
+
+async function getRouteNames()
+{
+  const response = await fetch("https://data.texas.gov/download/r4v4-vz24/application%2Fzip");
+  const { headers } = response;
+  const contentType = headers.get("content-type") || "";
+  if (contentType.includes("application/octet-stream"))
+  {
+    try
+    {
+      const zip = new JSZip();
+      const zipData = await response.arrayBuffer();
+      const zipPayload = await zip.loadAsync(zipData);
+      const tripsFile = zipPayload.file("trips.txt");
+      if (tripsFile !== null)
+      {
+        const tripsCsv = await tripsFile.async("string");
+        const tripsCsvLines = tripsCsv.split("\n");
+
+        if (tripsCsvLines.length > 1)
+        {
+          const headerFields = tripsCsvLines[0].trim().split(",");
+          const routeIdIndex = headerFields.indexOf("route_id");
+          const directionIdIndex = headerFields.indexOf("direction_id");
+          const tripShortNameIndex = headerFields.indexOf("trip_short_name");
+
+          const routeNames: Map<string, Map<number, string>> = new Map();
+          for (const line of tripsCsvLines.slice(1))
+          {
+            const fields = line.trim().split(",");
+            const routeId = fields[routeIdIndex];
+            const directionId = parseInt(fields[directionIdIndex]);
+            const tripShortName = fields[tripShortNameIndex];
+            
+            var directions = routeNames.get(routeId);
+            if (directions === undefined)
+            {
+              directions = new Map();
+              routeNames.set(routeId, directions);
+            }
+
+            directions.set(directionId, tripShortName);
+          }
+
+          const niceRoutes = Array.from(routeNames, ([routeId, directions]) => {
+            const niceDirections = Array.from(directions, ([directionId, name]) => {
+              return { directionId, name };
+            });
+            return { routeId, directions: niceDirections };
+          });
+
+          return { ok: true, routes: niceRoutes };
+        }
+      }
+    }
+    catch (e)
+    {
+    }
+  }
+
+  return { ok: false, routes: null };
+}
+
+async function getWeather(station: string, apiKey: string) {
+  const response = await fetch(`https://api.weather.gov/stations/${station}/observations/latest`, {
+    headers: {
+      "user-agent": apiKey
+    }
+  });
+  const { headers } = response;
+  const contentType = headers.get("content-type") || "";
+  if (contentType.includes("application/geo+json"))
+  {
+    try
+    {
+      const payload = await response.json() as any;
+      const { properties } = payload;
+      if (properties !== undefined)
+      {
+        const { textDescription, temperature } = properties;
+        if (textDescription !== undefined && temperature !== undefined)
+        {
+          const { value } = temperature;
+          if (value !== undefined)
+          {
+            return { ok: true, weather: {description: textDescription, temperature: value } };
+          }
+        }
+      }
+    }
+    catch (e)
+    {
+    }
+  }
+
+  return { ok: false, weather: null };
+}
+
 export default {
-  fetch(request) {
+  async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
+    if (url.pathname.startsWith("/bustimes")) {
+      const stops = url.searchParams.get("stops");
+      if (stops !== null)
+      {
+        const stopArray = stops.split(",");
+        return Response.json(await getBusTimes(stopArray));
+      }
+      return new Response(null, { status: 400 });
+    }
+    else if (url.pathname.startsWith("/routenames")) {
+      return Response.json(await getRouteNames());
+    }
+    else if (url.pathname.startsWith("/weather")) {
+      const station = url.searchParams.get("station");
+      if (station !== null)
+      {
+        return Response.json(await getWeather(station, env.WEATHER_API_KEY));
+      }
+      return new Response(null, { status: 400 });
     }
 		return new Response(null, { status: 404 });
   },
