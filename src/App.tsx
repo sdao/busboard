4877e8f51f0842503a1ad20a069e07c7825a771e
Steven Dao <stevedao@gmail.com>
2025-04-18 23:19:58 -0500

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import cloudflareLogo from './assets/Cloudflare_Logo.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import cloudflareLogo from './assets/Cloudflare_Logo.svg'
 import './App.css'
 
 type WeatherData = { description: string, temperature: number };
