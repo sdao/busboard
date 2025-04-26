3e403a5e0821cfee06f0eca71fd8200acdd96cf3
Steven Dao <stevedao@gmail.com>
2025-04-25 21:17:43 -0500

    abbreviations

--- src/App.tsx
+++ src/App.tsx
@@ -138,12 +138,61 @@ function BusTimeDisplay({ routeId, directionId, nextInstances, routeNames }: { r
     return () => window.clearInterval(intervalId);
   }, []);
 
+  function abbreviate(n: string)
+  {
+    // If `n` is all upper-case, convert it to title case
+    if (n.toUpperCase() === n)
+    {
+      const wordRegex = /\b\w+\b/g;
+      const matches = n.matchAll(wordRegex);
+
+      let titleCase = "";
+      for (const match of matches) {
+        const start = match.index;
+        const end = start + match[0].length;
+
+        for (let i = titleCase.length; i < start; ++i)
+        {
+          titleCase += n[i];
+        }
+
+        titleCase += n[start].toUpperCase();
+        for (let i = start + 1; i < end; ++i)
+        {
+          titleCase += n[i].toLowerCase();
+        }
+      }
+      
+      for (let i = titleCase.length; i < n.length; ++i)
+      {
+        titleCase += n[i];
+      }
+
+      n = titleCase;
+    }
+
+    // Abbreviate some common words
+    return n.replace(/\bAvenue\b/ig, "Av")
+            .replace(/\bBoulevard\b/ig, "Blvd")
+            .replace(/\bCenter\b/ig, "Ctr")
+            .replace(/\bPark.+Ride\b/ig, "P+R")
+            .replace(/\bParkway\b/ig, "Pkwy")
+            .replace(/\bPlace\b/ig, "Pl")
+            .replace(/\bPlaza\b/ig, "Plz")
+            .replace(/\bRoad\b/ig, "Rd")
+            .replace(/\bSquare\b/ig, "Sq")
+            .replace(/\bStation\b/ig, "Sta")
+            .replace(/\bStreet\b/ig, "St")
+            .replace(/\bUniversity\b/ig, "Univ")
+            ;
+  }
+
   let name = `Route ${routeId}`;
   const route = routeNames.routes.find((route) => route.routeId == routeId);
   if (route !== undefined) {
     const dir = route.directions.find((dir) => dir.directionId == directionId);
     if (dir !== undefined) {
-      name = dir.name;
+      name = abbreviate(dir.name);
     }
   }
 
